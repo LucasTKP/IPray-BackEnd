b6a724a659a47a3e3c7ccddd446f7ee2e63f7e76
Lucas Gean dos Santos <lucasgeansantos@gmail.com>
2024-04-30 19:56:13 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,6 @@ app.use(cookieParser());
 
 app.use("/v1/praies", praiesRouter);
 app.use("/v1/users", usersRouter);
-app.use('', () => console.log('aaa'));
+app.use('/', (req, res) => res.send("Sucesso");
 
 export default app;
