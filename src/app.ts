c9c9c46da68d23be986fa228b3d6c3669511ce62
Lucas Gean dos Santos <lucasgeansantos@gmail.com>
2024-04-30 20:04:18 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { json, urlencoded } from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
+import { Router } from "express";
 
 import praiesRouter from "./routes/praies";
 import usersRouter from "./routes/users";
 
 const app = express();
+const router = Router();
 
 app.use(logger("dev"));
 app.use(json());
@@ -14,6 +16,6 @@ app.use(cookieParser());
 
 app.use("/v1/praies", praiesRouter);
 app.use("/v1/users", usersRouter);
-app.use('/', (req, res) => res.send("Sucesso"));
+app.use('/', router.get('', (req, res) => res.send("Sucesso")));
 
 export default app;
